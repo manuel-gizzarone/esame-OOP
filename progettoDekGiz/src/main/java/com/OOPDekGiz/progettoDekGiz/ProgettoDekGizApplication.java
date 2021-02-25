bd8edf55f9cc9a0602ecfc2cba839e148d2ff3b4
edecaro <emanuele.studio01@gmail.com>
2021-02-26 00:13:07 +0100

    primo commit

---
+++ progettoDekGiz/src/main/java/com/OOPDekGiz/progettoDekGiz/ProgettoDekGizApplication.java
@@ -0,0 +1,13 @@
+package com.OOPDekGiz.progettoDekGiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoDekGizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoDekGizApplication.class, args);
+	}
+
+}
---
+++ progettoDekGiz/src/test/java/com/OOPDekGiz/progettoDekGiz/ProgettoDekGizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OOPDekGiz.progettoDekGiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoDekGizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
